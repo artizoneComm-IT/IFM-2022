8fcb288672bb5ff02ad555fa7b3925c4594beb85
lahatra3 <lahatrap20.aps2a@gmail.com>
2022-10-20 17:10:54 +0300

    swagger...

--- backdend/src/main.ts
+++ backdend/src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Zarao')
+    .setDescription('API de Zarao')
+    .setVersion('1.0')
+    .addTag('iTeam-$')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
   app.enableCors();
   await app.listen(3131);
 }
